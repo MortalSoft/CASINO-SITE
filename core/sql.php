d339b8b45fe3c0ebbf4e7e30857ed2a8f5bd0f40
mortalsoftdev <144179559+mortalsoftdev@users.noreply.github.com>
2023-09-06 00:37:57 +0200

    v1.0.0

--- core/sql.php
+++ core/sql.php
@@ -4,18 +4,12 @@
 		define("DB_HOST", "localhost"); 	// MySQL Database Host
 		define("DB_USER", "root");			// MySQL Username
 		define("DB_PASS", "");  			// MySQL Password
-		define("DB_NAME", "rgb_gpanel");  	// Database Name
+		define("DB_NAME", "");  	// Database Name
 	} else {
 		// Database for public
 		define("DB_HOST", "localhost"); 	// MySQL Database Host
-		define("DB_USER", "admin_demo");			// MySQL Username
-		define("DB_PASS", "oulL3iZfU");  	// MySQL Password
-		define("DB_NAME", "admin_demo");  		// Database Name
-	}
-
-       try {
-		$db = new PDO('mysql:host=localhost;dbname=admin_demo', 'admin_demo', 'oulL3iZfU', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	} catch (PDOException $e) {
-		exit($e->getMessage());
+		define("DB_USER", "");			// MySQL Username
+		define("DB_PASS", "");  	// MySQL Password
+		define("DB_NAME", "");  		// Database Name
 	}
 ?>
